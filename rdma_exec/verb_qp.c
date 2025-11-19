981e7615592cf045dd816b551883ff44e3715f16
Liuyi <liuy22@mails.tsinghua.edu.cn>
2025-11-19 12:30:53 +0800

    add warning of not implemented verbs

--- rdma_exec/verb_qp.c
+++ rdma_exec/verb_qp.c
@@ -1,22 +1,28 @@
 #include "verb_qp.h"
+#include <stdio.h>
 
 int handle_CreateQP(cJSON *verb_obj, ResourceEnv *env)
 {
+    fprintf(stderr, "[EXEC] CreateQP: not implemented yet\n");
     return 0;
 }
 int handle_ModifyQP(cJSON *verb_obj, ResourceEnv *env)
 {
+    fprintf(stderr, "[EXEC] ModifyQP: not implemented yet\n");
     return 0;
 }
 int handle_DestroyQP(cJSON *verb_obj, ResourceEnv *env)
 {
+    fprintf(stderr, "[EXEC] DestroyQP: not implemented yet\n");
     return 0;
 }
 int handle_PostSend(cJSON *verb_obj, ResourceEnv *env)
 {
+    fprintf(stderr, "[EXEC] PostSend: not implemented yet\n");
     return 0;
 }
 int handle_PostRecv(cJSON *verb_obj, ResourceEnv *env)
 {
+    fprintf(stderr, "[EXEC] PostRecv: not implemented yet\n");
     return 0;
 }
